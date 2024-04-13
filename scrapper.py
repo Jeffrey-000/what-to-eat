f46a8b9e47551fb94da7268ee4ce9e2e6ffb2c51
Jeffaaaaaah <jeffaaaaaah@gmail.com>
2024-04-12 23:35:11 -0400

    code reformating: make varaiables that should be staying constant -> constant globals

--- scrapper.py
+++ scrapper.py
@@ -9,14 +9,56 @@ class Today:
         self.MONTH = date.today().strftime("%m")
         self.DAY = date.today().strftime("%d")
 
-#making this a class is probably unessecary but will keep for now
-class Menu:
-    def __init__(self, data):
-        self.data = data
+
+#useful urls?
+#https://uga.api.nutrislice.com/menu/api/schools/
+
+#------------------------------------------^^^^ugli code--------------------------------------------------
+WHITE_LIST_CATAGORIES = ['Classic Cuisine', 
+                       'Fruit', 
+                       'Hickory & Oak Specials', 
+                       'Lemon Grass Kitchen Specials', 
+                       'Special Selections Specials', 
+                       "Chef's Choice", 
+                       'Deliciously Southern', 
+                       'Dessert Specials',
+                       "O'hacienda",
+                       'Snelling Selects',
+                       'The Bowl Specials',
+                       "Headliners"]
+
+WHITE_LIST_FOODS = []
+
+
+DINNING_HALLS = {"BOLTON" : "dining-hall-1",
+                "OHOUSE" : "dining-hall-2",
+                "SNELLING" : "dining-hall-3",
+                "NICHE" : "dining-hall-4",
+                "JOEFRANK" : "dining-hall-5"}
+
+MEALTYPE = ["breakfast", "lunch", "dinner"]
+
+class Bot:
+    def __init__(self):
+        self.today = Today()
+
+    #prob useless function but making just in case for now
+    def updateToday(self):
+        self.today = Today()
+
+    #return whole weeks of data
+    #returns json data in python dict/list form
+    #no error checking, assumes input is always valid    
+    def get_api(self, diningHall: str, breakfastLunchOrDinner: str):
+        url = f"https://uga.api.nutrislice.com/menu/api/weeks/school/{diningHall}/menu-type/{breakfastLunchOrDinner}/{self.today.YEAR}/{self.today.MONTH}/{self.today.DAY}/"
+        #print(url)
+        return requests.get(url).json()
     
-    def parseMenu(self):
+
+    #parses raw api data to only include catagories and a list of food that they contain
+    def parseMenu(self, data):
         days = {}
-        for day in self.data["days"]: #day is a dictionary object
+        for day in data["days"]: #day is a dictionary object
             if len(day["menu_info"]) != 0:
                 catagoryDic = {}
 
@@ -30,49 +72,43 @@ class Menu:
                         #print(menu_item)
                         #print('\n\n\n\n\n\n')
                         #sleep(20)
-                        if str(menu_item["menu_id"]) == catagory_id: #dictionary keys (catagory_id) are strings. to be evaluated all other values must be casted to string
+
+
+                        #dictionary keys (catagory_id) are strings. to be evaluated all other values must be casted to string
+                        #menu_id and caragory_id refer to the same data
+                        if str(menu_item["menu_id"]) == catagory_id: 
                             foodUnderThisCatagoryList.append(menu_item["food"]["name"])
                     catagoryDic[catagoryName] = foodUnderThisCatagoryList
             days[day['date']] = catagoryDic
 
         return days
-                    
+    
+    #takes in the return data of parseMenu()
+    #would be more algorithmicly efficient if integrate with parseMenu function but dont care rn
+    def filterMenu(self, data: dict):
+        filteredDic = {}
+        for key, value in data.items(): #key = date, value = {catagory : foodList}
+            tempValueDic = {}
+            for catagory, foodlist in value.items():
+                if catagory in WHITE_LIST_CATAGORIES:
+                    tempValueDic[catagory] = foodlist
 
+            filteredDic[key] = tempValueDic
 
-        
+        return filteredDic
+    
+    #TODO: since intergrating with discord, make function to bold foods in WHITE_LIST_FOODS
 
-
-
-
-#useful urls?
-#https://uga.api.nutrislice.com/menu/api/schools/
-
-#------------------------------------------^^^^ugli code--------------------------------------------------
-
-class Bot:
-    def __init__(self):
-        self.today = Today()
-        self.diningHalls = {"BOLTON" : "dining-hall-1",
-                            "OHOUSE" : "dining-hall-2",
-                            "SNELLING" : "dining-hall-3",
-                            "NICHE" : "dining-hall-4",
-                            "JOEFRANK" : "dining-hall-5"}
-        self.breakfastLunchOrDiner = ["breakfast", "lunch", "dinner"]
-        
-
-    #returns json data in python dict/list form
-    #no error checking, assumes input is always valid    
-    def get_api(self, diningHall: str, breakfastLunchOrDinner: str):
-        url = f"https://uga.api.nutrislice.com/menu/api/weeks/school/{diningHall}/menu-type/{breakfastLunchOrDinner}/{self.today.YEAR}/{self.today.MONTH}/{self.today.DAY}/"
-        #print(url)
-        return requests.get(url).json()
 
 
         
 def testing():
     bot = Bot()
-    menu = Menu(bot.get_api(bot.diningHalls["BOLTON"], bot.breakfastLunchOrDiner[0]))
-    print(menu.parseMenu())
+    data = bot.get_api(DINNING_HALLS["BOLTON"], 'lunch')
+    data = bot.parseMenu(data)
+    print(data[bot.today.TODAY])
+    print("\n\n\n\n\n\n\n\n\n")
+    print(bot.filterMenu(data))
 
 
 
