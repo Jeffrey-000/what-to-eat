044474614b4faa085861605238436bdc86e99a04
Jeffrey <jeffaaaaaah@gmail.com>
2024-09-02 17:43:44 -0400

    added funny text formating

--- discordBot.py
+++ discordBot.py
@@ -5,6 +5,7 @@ import scrapper
 import discord
 from dotenv import load_dotenv
 import os
+import random
 load_dotenv()
 
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -16,6 +17,18 @@ bot = discord.Client(intents=intents)
 
 food = scrapper.Bot()
 
+EMOJILIST = []
+EMOJILISTPATH = "/Users/moose/Documents/code/what-to-eat/emojiList.txt"
+with open(EMOJILISTPATH, "r") as f:
+    EMOJILIST = f.read().split("\n")
+
+
+def pretty(foodsList): #custom formatting for food string
+    s = ""
+    for string in foodsList:
+        s = s + string + " " + str(random.sample(EMOJILIST, 1)[0]) + " "
+
+    return s    
 
 #very inefficient sending
 #takes around 40 secs just sending
@@ -27,17 +40,19 @@ async def on_ready():
             await channel.purge()
             await channel.send("--------------------------------------------------------**LUNCH**--------------------------------------------------------", silent=True)
             data  = food.filterParsedMenu(food.parseMenuWholeWeek(food.get_api(scrapper.DINNING_HALLS[str(channel).upper()], 'lunch')))[food.today.TODAY]
+            #with open("./test.txt","w") as f:
+               # f.write(str(data))
             for key, value in data.items():
-                await channel.send("**" + str(key)  + "**" + "\n" + str(value) + "\n.", silent=True)
+                await channel.send("**" + str(key)  + "**" + "\n" + pretty(value) + "\n.", silent=True)
 
             await channel.send("--------------------------------------------------------**DINNER**--------------------------------------------------------", silent=True)
 
             data  = food.filterParsedMenu(food.parseMenuWholeWeek(food.get_api(scrapper.DINNING_HALLS[str(channel).upper()], 'dinner')))[food.today.TODAY]
             for key, value in data.items():
-                await channel.send("**" + str(key)  + "**" + "\n" + str(value) + "\n.", silent=True)
+                await channel.send("**" + str(key)  + "**" + "\n" + pretty(value) + "\n.", silent=True)
 
     await bot.close() #does print some unclosed connection warning but its a dont care situation for now
-
+    exit()
 
 @bot.event
 async def on_message(message):
